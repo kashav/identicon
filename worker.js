6f1c41f7fa31a8ed3094feb6dcb14bf2e0e5c83a
Kashav Madan <kshvmdn@gmail.com>
2024-07-24 04:12:55 +0000

    Run prettier on the source files

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 self.importScripts(
-  "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/core.min.js"
+  "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/core.min.js",
 );
 
 self.importScripts(
-  "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/md5.min.js"
+  "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/md5.min.js",
 );
 
 // adapted from https://stackoverflow.com/a/29433028
